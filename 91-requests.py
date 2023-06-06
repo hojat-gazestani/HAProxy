5ae4313f51594c0ef46d704d24f513443bddd7ae
hojat-gazestani <HojatGazestani@gmail.com>
2023-06-06 10:13:34 +0330

    12

--- 91-requests.py
+++ 91-requests.py
@@ -2,11 +2,7 @@ import requests
 import threading
 
 def send_requests():
-<<<<<<< HEAD
     url = 'http://192.168.56.21:81'
-=======
-    url = 'http://192.16.56.21:81'
->>>>>>> 87515f4fc3dd6a76497dd3a19355def92714fb00
 
     for _ in range(1000):
         threading.Thread(target=send_request, args=(url,)).start()
