44890b75e0f152734ff94f14ec863c9f5f4c6548
hojat-gazestani <HojatGazestani@gmail.com>
2023-06-06 22:41:03 +0330

    12

--- 95-show1.py
+++ 95-show1.py
@@ -1,5 +1,30 @@
 #!/usr/bin/python3
 
+# from haproxyadmin import haproxy
+
+# hap = haproxy.HAProxy(socket_file='/run/haproxy/admin.sock')
+
+# frontends = hap.frontends()
+# backends = hap.backends()
+
+# for frontend in frontends:
+#     if frontend.name == 'Italy':
+#         print("Frontend: {}".format(frontend.name))
+#         backend_name = frontend.default_backend
+#         for backend in backends:
+#             if backend.name == backend_name:
+#                 for server in backend.servers():
+#                     print("  Server: {} ({})".format(server.name, server.address))
+#                 break
+
+# for backend in backends:
+#     if backend.name == 'myapps':
+#         print("Backend: {}".format(backend.name))
+#         for server in backend.servers():
+#             print("  Server: {} ({})".format(server.name, server.address))
+
+#!/usr/bin/python3
+
 from haproxyadmin import haproxy
 
 hap = haproxy.HAProxy(socket_file='/run/haproxy/admin.sock')
@@ -10,15 +35,21 @@ backends = hap.backends()
 for frontend in frontends:
     if frontend.name == 'Italy':
         print("Frontend: {}".format(frontend.name))
-        backend_name = frontend.default_backend
-        for backend in backends:
-            if backend.name == backend_name:
-                for server in backend.servers():
-                    print("  Server: {} ({})".format(server.name, server.address))
+        backend_name = None
+        for line in frontend.raw_data.split('\n'):
+            if line.startswith('default_backend'):
+                backend_name = line.split(' ')[1]
                 break
+        if backend_name:
+            for backend in backends:
+                if backend.name == backend_name:
+                    for server in backend.servers():
+                        print("  Server: {} ({})".format(server.name, server.address))
+                    break
 
 for backend in backends:
     if backend.name == 'myapps':
         print("Backend: {}".format(backend.name))
         for server in backend.servers():
             print("  Server: {} ({})".format(server.name, server.address))
+
